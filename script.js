3d17f840a3d9e627b058b9558b8c12ffe4e8979f
Beka Bantasdze <thelataito@gmail.com>
2022-11-09 20:41:57 +0400

    q1

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
+var menuToggle = document.querySelector("#menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+var toggledMenu = menuToggle.addEventListener("click", function(){
+
+     for(var activated = 0; activated < activeElements.length; activated++){
+          activeElements[activated].classList.toggle("active");
+     }
+})
+
+document.getElementById("clickBody")
+  .addEventListener("click", function (event) {
+    console.log(event);
+  });
+
+document.getElementById("username").addEventListener("keydown", function (y) {
+
+  y.target.classList.add("input-wraper");
+});
+
+document.getElementById("link").addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+
+let usernameField = document.getElementById("username");
+
+usernameField.addEventListener("input", function (event) {
+  event.target.value = event.target.value.toUpperCase();
+});
+
+let ulElement = document.querySelector('.ul-list');
+
+let newLiElement = document.createElement('li');
+newLiElement.textContent = 'javascript';
+// newLiElement.innerText = 'javascript';
+newLiElement.classList.add('li-item');
+
+let newLiElement2 = document.createElement('li');
+newLiElement2.textContent = 'html';
+
+ulElement.appendChild(newLiElement);
+ulElement.appendChild(newLiElement2);
